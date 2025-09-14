6068bc93587d78cb2c82e4431501c60f39658ae4
yunngaeng <yungyeong04@gmail.com>
2025-09-14 16:06:11 +0900

    Add files via upload

---
+++ ch01/sec08/Hello.java
@@ -0,0 +1,11 @@
+package ch01.sec08;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ch02/sec01/VarableInitializationExample.java
@@ -0,0 +1,17 @@
+package ch02.sec01;
+
+public class VarableInitializationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수 선언
+		int value=0;
+		
+		//연산 결과를 변수의 초기값으로 대입
+		int result=value+10;
+		
+		//변수 값을 읽고 콘솔에 출력
+		System.out.println(result);
+	}
+
+}
