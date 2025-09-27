a2acbea26384a008fdf011acb8528d712a4170ef
yunngaeng <yungyeong04@gmail.com>
2025-09-27 17:36:17 +0900

    Add files via upload

---
+++ ch04/sec05/SumFromTo1To10Example.java
@@ -0,0 +1,17 @@
+package ch04.sec05;
+
+public class SumFromTo1To10Example {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+	int i =1;
+		
+		while(i<=100) {
+			sum += i;
+			i++;
+		}
+		System.out.println("1~" + (i-1)+ " 합 : " + sum);
+	}
+
+}
---
+++ test/test.java
@@ -0,0 +1,23 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 1000000;
+		int y = 1000000;
+		int z = x*y;
+		
+		System.out.println(z);
+	
+		System.out.println("--------------------");
+		
+		long x1 = 1000000;
+		long y1 = 1000000;
+		long z1 = (long)x*(long)y;
+		
+		System.out.println(z1);
+	}
+
+}
